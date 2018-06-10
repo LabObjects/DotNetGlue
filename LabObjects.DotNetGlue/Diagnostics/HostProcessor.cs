d4db0d444683bbeb4694256dd5e240915578c995
April Daly <adaly@labobjects.com>
2018-06-09 20:50:16 -0400

    testing repo

--- LabObjects.DotNetGlue/Diagnostics/HostProcessor.cs
+++ LabObjects.DotNetGlue/Diagnostics/HostProcessor.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-using System.Timers;
 using LabObjects.DotNetGlue.SharedKernel;
 
 namespace LabObjects.DotNetGlue.Diagnostics
